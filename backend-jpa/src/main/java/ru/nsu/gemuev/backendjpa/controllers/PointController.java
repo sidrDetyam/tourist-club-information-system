b31eeffc71791885130621ad81c9465d55e881db
sidrDetyam <a.gemuev@g.nsu.ru>
2023-05-02 00:15:15 +0700

    bruh

--- backend-jpa/src/main/java/ru/nsu/gemuev/backendjpa/controllers/PointController.java
+++ backend-jpa/src/main/java/ru/nsu/gemuev/backendjpa/controllers/PointController.java
@@ -1,11 +1,47 @@
 package ru.nsu.gemuev.backendjpa.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.nsu.gemuev.backendjpa.dto.IdDto;
+import ru.nsu.gemuev.backendjpa.dto.PointDto;
+import ru.nsu.gemuev.backendjpa.dto.requests.CreatePointRequest;
+import ru.nsu.gemuev.backendjpa.services.PointsService;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/hikes")
+@RequestMapping("/points")
 public class PointController {
+    private final PointsService pointsService;
+
+    @PostMapping("/get-by-id")
+    public ResponseEntity<PointDto> getTrainerById(@RequestBody IdDto request){
+        return new ResponseEntity<>(pointsService.getPointById(request.getId()), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Void> getTrainerById(@RequestBody CreatePointRequest request){
+        pointsService.createPoint(request);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<Void> delete(@RequestBody IdDto request){
+        pointsService.deletePoint(request.getId());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/edit")
+    public ResponseEntity<Void> edit(@RequestBody PointDto request){
+        pointsService.editPoint(request);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<PointDto>> getAll(){
+        return new ResponseEntity<>(pointsService.getAllPoints(), HttpStatus.OK);
+    }
 }
--- backend-jpa/src/main/java/ru/nsu/gemuev/backendjpa/services/PointsService.java
+++ backend-jpa/src/main/java/ru/nsu/gemuev/backendjpa/services/PointsService.java
@@ -3,7 +3,11 @@ package ru.nsu.gemuev.backendjpa.services;
 import jakarta.transaction.Transactional;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 import ru.nsu.gemuev.backendjpa.dto.PointDto;
 import ru.nsu.gemuev.backendjpa.dto.requests.CreatePointRequest;
@@ -20,22 +24,22 @@ import java.util.stream.StreamSupport;
 public class PointsService {
     private final PointsRepository pointsRepository;
     private final PointMapper pointMapper;
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Transactional
-    public @NonNull PointDto getPointById(final long id){
+    public @NonNull PointDto getPointById(final long id) {
         return pointMapper.toDto(pointsRepository.findById(id).orElseThrow());
     }
 
     @Transactional
-    public @NonNull List<PointDto> getAllPoints(){
+    public @NonNull List<PointDto> getAllPoints() {
         return StreamSupport.stream(pointsRepository.findAll().spliterator(), false)
                 .map(pointMapper::toDto)
                 .toList();
     }
 
     @Transactional
-    public void createPoint(@NonNull CreatePointRequest request){
+    public void createPoint(@NonNull CreatePointRequest request) {
         RequestFieldChecker.requireNonNull(request.getPoint());
         RequestFieldChecker.requireNonNull(request.getDescription());
         final var point = new ControlPoint();
@@ -45,7 +49,25 @@ public class PointsService {
     }
 
     @Transactional
-    public void deletePoint(final long id){
+    public void deletePoint(final long id) {
+        final KeyHolder holder = new GeneratedKeyHolder();
+        final SqlParameterSource parameters = new MapSqlParameterSource()
+                .addValue("id", id);
 
+        namedParameterJdbcTemplate.update(
+                "delete from routes_control_points where point_id=:id; " +
+                "delete from control_points where id=:id", parameters, holder);
+    }
+
+    @Transactional
+    public void editPoint(final @NonNull PointDto request){
+        RequestFieldChecker.requireNonNull(request.getPoint());
+        RequestFieldChecker.requireNonNull(request.getDescription());
+        RequestFieldChecker.requireNonNull(request.getId());
+
+        final ControlPoint point = pointsRepository.findById(request.getId()).orElseThrow();
+        point.setDescription(request.getDescription());
+        point.setPoint(request.getPoint());
+        pointsRepository.save(point);
     }
 }
