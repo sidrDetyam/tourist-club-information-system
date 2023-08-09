49afa1e02f34520eb612c785979871eb5a86423a
Alexander Gemuev <91963508+sidrDetyam@users.noreply.github.com>
2023-08-10 03:56:04 +0700

    removed SD JDBC

--- backend/src/main/java/ru/nsu/gemuev/backend/security/dto/JwtLoginRequestDto.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.gemuev.backend.security.dto;
-
-import lombok.*;
-
-@Value
-public class JwtLoginRequestDto {
-    @NonNull String username;
-    @NonNull String password;
-}
--- backend/src/main/java/ru/nsu/gemuev/backend/security/dto/JwtRefreshResponseDto.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.gemuev.backend.security.dto;
-
-import lombok.NonNull;
-import lombok.Value;
-
-@Value
-public class JwtRefreshResponseDto {
-    @NonNull String accessToken;
-}
